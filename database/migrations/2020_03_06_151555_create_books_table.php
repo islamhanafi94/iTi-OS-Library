6b1770a069f67dd4d780aa13386d534eca46ce0b
islamhanafi94 <islamhanafi1994@gmail.com>
2020-03-06 17:58:59 +0200

    users,categories,books migration

---
+++ database/migrations/2020_03_06_151555_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->string('author',100);
+            $table->integer('stock');
+            $table->integer('available_copies');
+            $table->float('rating',3,2,true);
+            $table->unsignedBigInteger('category_id',100);
+            $table->text('description');
+            $table->string('image');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
