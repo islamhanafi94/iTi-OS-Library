47c8836bf5e59c712ea633b9a15236fc6395fe6a
MacrinaAshraf <macrina1997@gmail.com>
2020-03-07 17:24:00 +0200

    isAdmin middleware is created.
    Co-authored-by: DaliaMatter <daliaamohamed243@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -14,6 +15,8 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('CheckUser');
+        
     }
 
     /**
@@ -23,6 +26,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if(Auth::user()->is_active) 
+        {
+            // return hello;
+            return view('home');
+        }
+        else
+        {
+            Auth::logout();
+            return view('welcome');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('manager','ManagerController')->middleware('auth');
+// Route::resource('manager','ManagerController')->middleware('auth');
