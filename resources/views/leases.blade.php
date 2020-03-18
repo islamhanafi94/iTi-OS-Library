d599b5e67000bbb5ab50417a26abcf89279221e3
MacrinaAshraf <macrina1997@gmail.com>
2020-03-18 15:00:42 +0200

    finished My Books UI

--- resources/views/leases.blade.php
+++ resources/views/leases.blade.php
@@ -14,25 +14,3 @@
         </div>
     </div>
 @endsection
-
-{{-- @isset($books)
-@foreach ($books as $book)
-    <div class="books-card">
-        <div class="book-header">{{$book->title}}</div>
-        <div class="book-body">{{$book->description}}</div>
-        <div class="book-footer">{{$book->lease_price_per_day}}$</div>
-        <form action={{route("favorites.destroy",[$book->id])}} method="POST">
-            @csrf
-            @method('DELETE')
-            <input type="submit" value="delete from favorites">
-            <input type="hidden" name="user_id" value={{$book->pivot->user_id}}>
-            <input type="hidden" name="book_id" value={{$book->pivot->book_id}}>
-        </form>
-    </div>
-@endforeach
-@endisset
-@if(count($books) < 1)
-<div class="empty-books">
-no books avilable...
-</div>
-@endif --}}
