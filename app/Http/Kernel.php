d413490df2aed6508a84212ec4690c922c90ab95
DaliaMatter <daliaamohamed243@gmail.com>
2020-03-19 15:38:54 +0200

    fix issues in Admin middelware.
    Co-authored-by: MacrinaAshraf <macrina1997@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'CheckUser' => \App\Http\Middleware\CheckUser::class,
         'checkActive' => \App\Http\Middleware\checkActive::class,
+        'admin' => \App\Http\Middleware\Admin::class,
     ];
 }
